be7818205466da8e12544deb1974506f253eefd6
nakitamray <nakitamray@gmail.com>
2022-01-21 21:02:39 -0800

    navbar

---
+++ homepage/navbar.js
@@ -0,0 +1,26 @@
+// Responsive NavBar JS code included with line hover animation on nav bar links
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click',()=>{
+        //Toogle Nav
+        nav.classList.toggle('nav-active');
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+        //Burger Animation
+        burger.classList.toggle('toogle');
+        
+    });
+}
+
+navSlide();
