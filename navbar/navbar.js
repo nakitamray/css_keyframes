10facdf96635151617ae6cbd8a104bd0cad522fb
nakitamray <nakitamray@gmail.com>
2022-01-23 13:41:39 -0800

    animate links

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -10,13 +10,13 @@ const navSlide = () => {
         nav.classList.toggle('nav-active');
 
         //Animate Links
-        // navLinks.forEach((link, index) => {
-        //     if (link.style.animation) {
-        //         link.style.animation = '';
-        //     } else {
-        //         link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-        //     }
-        // });
+        navLinks.forEach((link, index) => {
+             if (link.style.animation) {
+                 link.style.animation = '';
+             } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
         
         //Burger Animation
         burger.classList.toggle('toogle');
